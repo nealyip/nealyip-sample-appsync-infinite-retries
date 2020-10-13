9609239d54f5b3f4608f7b907c0a4c48fff3afa7
faiyip pc <faiyip@gmail.com>
2020-10-13 21:25:39 +0800

    initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,9 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import AWSAppSyncClient, {AWSAppSyncClientOptions} from 'aws-appsync';
+import {default as randomQuery} from './query.gql';
+import {FormGroup, FormControl} from '@angular/forms';
+import options from './options';
+
 
 @Component({
   selector: 'app-root',
@@ -7,4 +12,45 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'sample-appsync';
+  errorMessage: string;
+  loading = false;
+
+  form: FormGroup = new FormGroup({
+    code: new FormControl()
+  });
+
+  options = options;
+
+  submit(): void {
+    this.query(this.form.get('code').value);
+  }
+
+  query(code: number): void {
+
+    this.loading = true;
+    this.errorMessage = null;
+    const args = {
+      url: '/graphql/' + code,
+      region: 'ap-northeast-1',
+      auth: {
+        type: 'OPENID_CONNECT',
+        jwtToken: async () => '1234'
+      },
+      offlineConfig: {
+        keyPrefix: Math.random().toString()
+      }
+    } as AWSAppSyncClientOptions;
+    const theClient = new AWSAppSyncClient(args);
+
+    theClient.hydrated().then(async (client: AWSAppSyncClient<any>) => {
+      return client.query({
+        query: randomQuery
+      });
+    }).then(r => this.loading = false).catch(err => {
+      console.error('Caught', err.message);
+      this.loading = false;
+      // Do sth to recover the error
+      this.errorMessage = err.message;
+    });
+  }
 }
