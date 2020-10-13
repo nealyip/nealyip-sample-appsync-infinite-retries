d2831f1621158c55c0268a49173aeb6a02c4cbb2
faiyip pc <faiyip@gmail.com>
2020-10-13 22:19:17 +0800

    Add a mode picker

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,7 @@
 import {Component, OnInit} from '@angular/core';
 import AWSAppSyncClient, {AWSAppSyncClientOptions} from 'aws-appsync';
 import {default as randomQuery} from './query.gql';
+import {default as randomMutation} from './mutation.gql';
 import {FormGroup, FormControl} from '@angular/forms';
 import options from './options';
 
@@ -10,22 +11,27 @@ import options from './options';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'sample-appsync';
   errorMessage: string;
   loading = false;
 
   form: FormGroup = new FormGroup({
-    code: new FormControl()
+    code: new FormControl(),
+    mode: new FormControl(),
   });
 
   options = options;
 
-  submit(): void {
-    this.query(this.form.get('code').value);
+  ngOnInit(): void {
+    this.form.get('mode').setValue('mutation');
   }
 
-  query(code: number): void {
+  submit(): void {
+    this.query(this.form.get('mode').value, this.form.get('code').value);
+  }
+
+  query(mode: 'query' | 'mutation', code: number): void {
 
     this.loading = true;
     this.errorMessage = null;
@@ -43,8 +49,13 @@ export class AppComponent {
     const theClient = new AWSAppSyncClient(args);
 
     theClient.hydrated().then(async (client: AWSAppSyncClient<any>) => {
+      if (mode === 'query') {
+        return client.query({
+          query: randomQuery
+        });
+      }
       return client.mutate({
-        mutation: randomQuery
+        mutation: randomMutation
       });
     }).then(r => this.loading = false).catch(err => {
       console.error('Caught', err.message);
